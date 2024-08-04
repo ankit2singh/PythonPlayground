24a0bdd63f9c3a44a6c189f22a0bead678159a98
username <ankitsr.77@gmail.com>
2024-08-04 18:49:40 +0530

    this is a commit

---
+++ 3_3.py
@@ -0,0 +1,12 @@
+# ***
+# ***
+# ***
+
+
+def pattern(n):
+ #n=int(input())
+ for i in range(n):
+    print(n*"*")
+
+n=int(input())
+pattern(n)    
---
+++ list.py
@@ -0,0 +1,5 @@
+#list data structure
+
+mylistq = [3,4,2,1,44,5,3]
+
+print(mylistq)

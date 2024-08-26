a3397f31d8f1b97b0313ed0b92843b50f4a81ed8
username <ankitsr.77@gmail.com>
2024-08-26 10:58:17 +0530

    rec

--- Recursion/Basic/factorial_recursion.py
+++ Recursion/Basic/factorial_recursion.py
@@ -1,20 +1,24 @@
-def fact(n):
-    ans = 1
-    i = 1
-    while(i<=n):
-        ans = ans*i
-        i += 1
-    return ans
+
+# # without recursion
+
+# def fact(n):
+#     ans = 1
+#     i = 1
+#     while(i<=n):
+#         ans = ans*i
+#         i += 1
+#     return ans
 
 
-print(fact(6))    
+# print(fact(6))    
 
 
 
-# # recursion
-# def factorial_rec(n):
-#     if n==0:
-#         return factorial_rec(n)*factorial_rec(n-1)
+# recursion
+def factorial_rec(n):
+    if n==0:
+        return 1
+    return n*factorial_rec(n-1)
     
 
-# print(factorial_rec(6))    
+print(factorial_rec(6))    

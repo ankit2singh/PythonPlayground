efca4d5e8a252f200f1baa192701fc95c6cffa06
Ankit Kumar Singh <107424618+ankit2singh@users.noreply.github.com>
2025-06-26 06:03:47 +0000

    arm

--- BasicModule/ArmstrongNumber.py
+++ BasicModule/ArmstrongNumber.py
@@ -1,9 +1,10 @@
 def Armstrong(n):
     num_og = n
     result = 0
+    degree = len(str(n))
     while n > 0:
         digit = n%10
-        result = result + digit**3
+        result = result + digit**degree
         n = n//10
 
     if result == num_og:
@@ -12,3 +13,7 @@ def Armstrong(n):
         return False
 
 print(Armstrong(333)) #153 and 1634 are the armstrong number
+
+print(Armstrong(153)) 
+
+print(Armstrong(1634)) 

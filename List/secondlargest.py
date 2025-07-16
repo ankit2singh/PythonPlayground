a5dc6f654e5961ee87139031048e1e7c2c655fd8
Ankit Kumar Singh <107424618+ankit2singh@users.noreply.github.com>
2025-07-16 13:00:47 +0530

    Update secondlargest.py

--- List/secondlargest.py
+++ List/secondlargest.py
@@ -1,1 +1,20 @@
+def getSecondOrderElements(a):
+    
 
+    largest= float("-inf")
+    s_largest= float("-inf")
+
+    for i in range(0, len(a)):
+
+        if a[i] > largest:
+
+            s_largest = largest
+
+            largest = a[i]
+        
+        if a[i] > s_largest and a[i] != largest:
+            s_largest = a[i]
+        
+    return [s_largest]    
+
+print(getSecondOrderElements([3,4,5,6,6]))

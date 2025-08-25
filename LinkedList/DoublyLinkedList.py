f444cea4c596519e9287ea29ff901db01d6d8555
Ankit Kumar Singh <107424618+ankit2singh@users.noreply.github.com>
2025-08-25 20:52:54 +0530

    Update DoublyLinkedList.py

--- LinkedList/DoublyLinkedList.py
+++ LinkedList/DoublyLinkedList.py
@@ -1,1 +1,21 @@
-class Node
+class Node:
+   def __init__(self, val):
+      self.val = val
+      self.next = None
+      self.next = None
+
+ class DoublyLinkedList:
+
+    def __init__(self):
+       self.head = None
+   
+    def insert_at_head(self, val):
+       new_node = self.val
+       if not self.head:
+         self.head = new_node
+       else:  
+         new_node.next = head
+         head.prev = new_node
+         head = new_node
+       
+    
